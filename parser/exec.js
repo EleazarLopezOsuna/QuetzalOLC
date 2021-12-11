d226268c05983a6e68a43b15e34c9238db1d0280
Eleazar Lopez <2997765060101@ingenieria.usac.edu.gt>
2021-12-11 00:23:59 -0600

    Added default function and 3 address code for string to int, float

--- parser/exec.js
+++ parser/exec.js
@@ -3989,16 +3989,13 @@ class native_function extends instruction_1.instruction {
 }
 exports.native_function = native_function;
 
-<<<<<<< HEAD
-},{"../abstract/instruction":5,"../system/error":43,"../system/type":44}],35:[function(require,module,exports){
-=======
-},{"../abstract/instruction":5,"../system/console":40,"../system/error":42,"../system/type":43}],34:[function(require,module,exports){
->>>>>>> 2cbb7edac1c574adb0da01b1ad49be97bb00aee6
+},{"../abstract/instruction":5,"../system/console":41,"../system/error":43,"../system/type":44}],35:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.native_parse = void 0;
 const error_1 = require("../system/error");
 const type_1 = require("../system/type");
+const console_1 = require("../system/console");
 const instruction_1 = require("../abstract/instruction");
 class native_parse extends instruction_1.instruction {
     constructor(native_type, value, line, column) {
@@ -4007,7 +4004,64 @@ class native_parse extends instruction_1.instruction {
         this.value = value;
     }
     translate(environment) {
-        throw new Error("Method not implemented.");
+        const dataType = this.value.translate(environment);
+        const dataTemp = console_1._3dCode.actualTemp;
+        let savedEnvironment = 0;
+        let resultTemp = 0;
+        switch (this.native_type) {
+            case type_1.type.INTEGER:
+                console_1._3dCode.actualTemp++;
+                savedEnvironment = console_1._3dCode.actualTemp;
+                console_1._3dCode.output += 'T' + savedEnvironment + ' = SP;//Save environment\n';
+                console_1._3dCode.output += 'SP = 29;//Set StringToInt environment\n';
+                console_1._3dCode.actualTemp++;
+                console_1._3dCode.output += 'T' + console_1._3dCode.actualTemp + ' = SP + 1;//Set string position\n';
+                console_1._3dCode.output += 'STACK[(int)T' + console_1._3dCode.actualTemp + '] = T' + dataTemp + ';//Save string\n';
+                console_1._3dCode.output += 'StringToInt();//Call function\n';
+                console_1._3dCode.actualTemp++;
+                resultTemp = console_1._3dCode.actualTemp;
+                console_1._3dCode.output += 'T' + console_1._3dCode.actualTemp + ' = SP + 0;//Set return position\n';
+                console_1._3dCode.actualTemp++;
+                console_1._3dCode.output += 'T' + console_1._3dCode.actualTemp + ' = STACK[(int)T' + resultTemp + '];//Get return value\n';
+                console_1._3dCode.output += 'SP = T' + savedEnvironment + ';//Recover environment\n';
+                return type_1.type.INTEGER;
+            case type_1.type.FLOAT:
+                console_1._3dCode.actualTemp++;
+                savedEnvironment = console_1._3dCode.actualTemp;
+                console_1._3dCode.output += 'T' + savedEnvironment + ' = SP;//Save environment\n';
+                console_1._3dCode.output += 'SP = 29;//Set StringToInt environment\n';
+                console_1._3dCode.actualTemp++;
+                console_1._3dCode.output += 'T' + console_1._3dCode.actualTemp + ' = SP + 1;//Set string position\n';
+                console_1._3dCode.output += 'STACK[(int)T' + console_1._3dCode.actualTemp + '] = T' + dataTemp + ';//Save string\n';
+                console_1._3dCode.output += 'StringToFloat();//Call function\n';
+                console_1._3dCode.actualTemp++;
+                resultTemp = console_1._3dCode.actualTemp;
+                console_1._3dCode.output += 'T' + console_1._3dCode.actualTemp + ' = SP + 0;//Set return position\n';
+                console_1._3dCode.actualTemp++;
+                console_1._3dCode.output += 'T' + console_1._3dCode.actualTemp + ' = STACK[(int)T' + resultTemp + '];//Get return value\n';
+                console_1._3dCode.output += 'SP = T' + savedEnvironment + ';//Recover environment\n';
+                return type_1.type.FLOAT;
+            case type_1.type.BOOLEAN:
+                console_1._3dCode.actualTag++;
+                const trueTag = console_1._3dCode.actualTag;
+                console_1._3dCode.actualTag++;
+                const falseTag = console_1._3dCode.actualTag;
+                console_1._3dCode.actualTag++;
+                const exitTag = console_1._3dCode.actualTag;
+                console_1._3dCode.actualTemp++;
+                console_1._3dCode.output += 'T' + console_1._3dCode.actualTemp + '= HEAP[(int)T' + dataTemp + '];//Get character\n';
+                console_1._3dCode.output += 'if(T' + console_1._3dCode.actualTemp + ' == 48) goto L' + trueTag + ';//Check if 0\n';
+                console_1._3dCode.output += 'goto L' + falseTag + ';\n';
+                console_1._3dCode.output += 'L' + trueTag + '://True tag\n';
+                console_1._3dCode.output += 'T' + console_1._3dCode.actualTemp + ' = 0;\n';
+                console_1._3dCode.output += 'goto L' + exitTag + ';\n';
+                console_1._3dCode.output += 'L' + falseTag + '://False tag\n';
+                console_1._3dCode.output += 'T' + console_1._3dCode.actualTemp + ' = 1;\n';
+                console_1._3dCode.output += 'goto L' + exitTag + ';\n';
+                console_1._3dCode.output += 'L' + exitTag + ':\n';
+                return type_1.type.BOOLEAN;
+        }
+        return type_1.type.NULL;
     }
     execute(environment) {
         let value_data = this.value.execute(environment);
@@ -4042,7 +4096,7 @@ class native_parse extends instruction_1.instruction {
 }
 exports.native_parse = native_parse;
 
-},{"../abstract/instruction":5,"../system/error":43,"../system/type":44}],36:[function(require,module,exports){
+},{"../abstract/instruction":5,"../system/console":41,"../system/error":43,"../system/type":44}],36:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.print = exports.print_type = void 0;
--- parser/instruction/native_parse.ts
+++ parser/instruction/native_parse.ts
@@ -2,14 +2,71 @@ import { expression } from "../abstract/expression";
 import { environment } from "../system/environment";
 import { error, error_arr, error_type } from "../system/error";
 import { data, type } from "../system/type";
-import { _console } from "../system/console";
+import { _console, _3dCode } from "../system/console";
 import { literal } from "../abstract/literal";
 import { instruction } from "../abstract/instruction";
 
 export class native_parse extends instruction {
 
     public translate(environment: environment): type {
-        throw new Error("Method not implemented.");
+        const dataType = this.value.translate(environment)
+        const dataTemp = _3dCode.actualTemp;
+        let savedEnvironment = 0;
+        let resultTemp = 0;
+        switch (this.native_type) {
+            case type.INTEGER:
+                _3dCode.actualTemp++;
+                savedEnvironment = _3dCode.actualTemp;
+                _3dCode.output += 'T' + savedEnvironment + ' = SP;//Save environment\n';
+                _3dCode.output += 'SP = 29;//Set StringToInt environment\n';
+                _3dCode.actualTemp++;
+                _3dCode.output += 'T' + _3dCode.actualTemp + ' = SP + 1;//Set string position\n';
+                _3dCode.output += 'STACK[(int)T' + _3dCode.actualTemp + '] = T' + dataTemp + ';//Save string\n';
+                _3dCode.output += 'StringToInt();//Call function\n';
+                _3dCode.actualTemp++;
+                resultTemp = _3dCode.actualTemp;
+                _3dCode.output += 'T' + _3dCode.actualTemp + ' = SP + 0;//Set return position\n';
+                _3dCode.actualTemp++;
+                _3dCode.output += 'T' + _3dCode.actualTemp + ' = STACK[(int)T' + resultTemp + '];//Get return value\n';
+                _3dCode.output += 'SP = T' + savedEnvironment + ';//Recover environment\n';
+                return type.INTEGER;
+            case type.FLOAT:
+                _3dCode.actualTemp++;
+                savedEnvironment = _3dCode.actualTemp;
+                _3dCode.output += 'T' + savedEnvironment + ' = SP;//Save environment\n';
+                _3dCode.output += 'SP = 29;//Set StringToInt environment\n';
+                _3dCode.actualTemp++;
+                _3dCode.output += 'T' + _3dCode.actualTemp + ' = SP + 1;//Set string position\n';
+                _3dCode.output += 'STACK[(int)T' + _3dCode.actualTemp + '] = T' + dataTemp + ';//Save string\n';
+                _3dCode.output += 'StringToFloat();//Call function\n';
+                _3dCode.actualTemp++;
+                resultTemp = _3dCode.actualTemp;
+                _3dCode.output += 'T' + _3dCode.actualTemp + ' = SP + 0;//Set return position\n';
+                _3dCode.actualTemp++;
+                _3dCode.output += 'T' + _3dCode.actualTemp + ' = STACK[(int)T' + resultTemp + '];//Get return value\n';
+                _3dCode.output += 'SP = T' + savedEnvironment + ';//Recover environment\n';
+                return type.FLOAT;
+            case type.BOOLEAN:
+                _3dCode.actualTag++
+                const trueTag = _3dCode.actualTag;
+                _3dCode.actualTag++
+                const falseTag = _3dCode.actualTag;
+                _3dCode.actualTag++
+                const exitTag = _3dCode.actualTag;
+                _3dCode.actualTemp++;
+                _3dCode.output += 'T' + _3dCode.actualTemp + '= HEAP[(int)T' + dataTemp + '];//Get character\n';
+                _3dCode.output += 'if(T' + _3dCode.actualTemp + ' == 48) goto L' + trueTag + ';//Check if 0\n';
+                _3dCode.output += 'goto L' + falseTag + ';\n';
+                _3dCode.output += 'L' + trueTag + '://True tag\n';
+                _3dCode.output += 'T' + _3dCode.actualTemp + ' = 0;\n';
+                _3dCode.output += 'goto L' + exitTag + ';\n';
+                _3dCode.output += 'L' + falseTag + '://False tag\n';
+                _3dCode.output += 'T' + _3dCode.actualTemp + ' = 1;\n';
+                _3dCode.output += 'goto L' + exitTag + ';\n';
+                _3dCode.output += 'L' + exitTag + ':\n';
+                return type.BOOLEAN;
+        }
+        return type.NULL
     }
 
     constructor(public native_type: type, public value: expression | literal, line: number, column: number) {
